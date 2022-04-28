5092f186e00cd0bc55108ac3deca85a2db0eba99
anthgrim <anthgrim@icloud.com>
2022-04-28 10:39:56 -0400

    Added Hero component and img grid collection

---
+++ airbnb-experiences-clone/src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+import image from "../images/imgCollection.jpg";
+
+const Hero = () => {
+  return (
+    <>
+      <img src={image} alt="imgCollection" />
+      <div className="header">
+        <h1 className="main-title">Online Experiences</h1>
+        <h3 className="sub-title">
+          Join unique interactive activities led by <br />
+          one-of-a-kind hosts-all without leaving <br />
+          home.
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
