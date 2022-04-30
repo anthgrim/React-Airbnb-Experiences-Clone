0eccae7347a8211357958958440ea28212b59757
anthgrim <anthgrim@icloud.com>
2022-04-29 22:51:23 -0400

    added props to card component

--- airbnb-experiences-clone/src/components/Card.js
+++ airbnb-experiences-clone/src/components/Card.js
@@ -1,25 +1,28 @@
 import React from "react";
-import swim from "../images/swim.jpg";
 import star from "../images/star.jpg";
 
-const Card = () => {
+const Card = ({ targetImg, rating, reviewCount, country, title, price }) => {
+  let imgPath = "../images/" + targetImg;
+  console.log(imgPath);
   return (
     <>
       <div className="card">
         <button className="card-btn">SOLD OUT</button>
-        <img src={swim} alt="swim" className="card-img" />
+        <img src={`../images/${targetImg}`} alt="swim" className="card-img" />
         <section className="card-content">
           <div className="row">
             <img src={star} alt="star" className="card-icon" />
-            <span className="score">5.0</span>
-            <span className="score review">(6) - USA</span>
+            <span className="score">{rating}</span>
+            <span className="score review">
+              {`(${reviewCount})`} - {country}
+            </span>
           </div>
           <div className="row">
-            <span className="description">Life lessons with Katie Zafares</span>
+            <span className="description">{title}</span>
           </div>
           <div className="row">
             <span className="price">
-              From $136/<span className="unit">person</span>{" "}
+              From ${price}/<span className="unit">person</span>{" "}
             </span>
           </div>
         </section>
