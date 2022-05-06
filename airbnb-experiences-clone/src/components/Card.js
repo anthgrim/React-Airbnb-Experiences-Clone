a35f52152bf81d33571cd0b90673a54002c3a733
Kevin Grimaldi <anthgrim@icloud.com>
2022-05-06 16:29:20 -0400

    Added mock data for cards and components map rendering

--- airbnb-experiences-clone/src/components/Card.js
+++ airbnb-experiences-clone/src/components/Card.js
@@ -1,34 +1,40 @@
 import React from "react";
 import star from "../images/star.jpg";
+import data from "../Data"
 
 const Card = ({ img, rating, reviewCount, country, title, price }) => {
   return (
     <>
-      <div className="card">
-        <button className="card-btn">SOLD OUT</button>
-        <img
-          src={require(`../images/${img}`)}
-          alt="swim"
-          className="card-img"
-        />
-        <section className="card-content">
-          <div className="row">
-            <img src={star} alt="star" className="card-icon" />
-            <span className="score">{rating}</span>
-            <span className="score review">
-              {`(${reviewCount})`} - {country}
-            </span>
+      {data.map((element) => {
+        return (
+          <div className="card">
+          <button className="card-btn">{element.openSpots === 0 ? "Sold Out" : "Book Now"}</button>
+          <img
+            src={require(`../images/${element.coverImg}`)}
+            alt="swim"
+            className="card-img"
+          />
+            <section className="card-content">
+              <div className="row">
+                <img src={star} alt="star" className="card-icon" />
+                <span className="score">{element.stats.rating}</span>
+                <span className="score review">
+                  {`(${element.stats.reviewCount})`} - {element.location}
+                </span>
+              </div>
+              <div className="row">
+                <span className="description">{element.title}</span>
+              </div>
+              <div className="row">
+                <span className="price">
+                  From ${element.price}/<span className="unit">person</span>{" "}
+                </span>
+              </div>
+            </section>
           </div>
-          <div className="row">
-            <span className="description">{title}</span>
-          </div>
-          <div className="row">
-            <span className="price">
-              From ${price}/<span className="unit">person</span>{" "}
-            </span>
-          </div>
-        </section>
-      </div>
+        )
+      })}
+      
     </>
   );
 };
