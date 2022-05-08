9dd06262d09a3003a304ed562a5ae303aaaf29a4
Kevin Grimaldi <anthgrim@icloud.com>
2022-05-07 22:02:45 -0400

    Completed card styles

--- airbnb-experiences-clone/src/components/Card.js
+++ airbnb-experiences-clone/src/components/Card.js
@@ -5,36 +5,38 @@ import data from "../Data"
 const Card = () => {
   return (
     <>
-      {data.map((element) => {
-        return (
-          <div className="card">
-          <button className="card-btn">{element.openSpots === 0 ? "Sold Out" : "Book Now"}</button>
-          <img
-            src={require(`../images/${element.coverImg}`)}
-            alt="swim"
-            className="card-img"
-          />
-            <section className="card-content">
-              <div className="row">
-                <img src={star} alt="star" className="card-icon" />
-                <span className="score">{element.stats.rating}</span>
-                <span className="score review">
-                  {`(${element.stats.reviewCount})`} - {element.location}
-                </span>
-              </div>
-              <div className="row">
-                <span className="description">{element.title}</span>
-              </div>
-              <div className="row">
-                <span className="price">
-                  From ${element.price}/<span className="unit">person</span>{" "}
-                </span>
-              </div>
-            </section>
-          </div>
-        )
-      })}
-      
+      <div className="card-container">
+        {data.map((element) => {
+          return (
+            <div className="card">
+            <button className="card-btn">{element.openSpots === 0 ? "Sold Out" : "Book Now"}</button>
+            <img
+              src={require(`../images/${element.coverImg}`)}
+              alt="swim"
+              className="card-img"
+            />
+              <section className="card-content">
+                <div className="row">
+                  <img src={star} alt="star" className="card-icon" />
+                  <span className="score">{element.stats.rating}</span>
+                  <span className="score review">
+                    {`(${element.stats.reviewCount})`} - {element.location}
+                  </span>
+                </div>
+                <div className="row">
+                  <span className="description">{element.title}</span>
+                </div>
+                <div className="row">
+                  <span className="price">
+                    From ${element.price}/<span className="unit">person</span>{" "}
+                  </span>
+                </div>
+              </section>
+            </div>
+          )
+        })}  
+      </div>
+        
     </>
   );
 };
